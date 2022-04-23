7825a5a27c4ebb773c8f5fa7bead7f0a66ea5564
Moez Missaoui <moeiz.missaoui@gmail.com>
2022-04-23 14:50:36 +0100

    Class definition

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,43 @@
-import 'package:oop_dart_application/oop_dart_application.dart'
-    as oop_dart_application;
+class Person {
+  late String name;
+  late int age;
+  late double salary;
+  late String city;
+  late String country;
+
+  void eat() {
+    print("$name is eating.");
+  }
+
+  void sleep() {
+    print("$name is sleeping.");
+  }
+
+  void watch() {
+    print("$name is watching.");
+  }
+
+  void showSalary() => print("$name salary's = $salary");
+
+  int getAge() {
+    return age;
+  }
+
+  String getName() => name;
+}
 
 void main(List<String> arguments) {
-  print('Hello world: ${oop_dart_application.calculate()}!');
+  var moez = Person();
+  moez.name = "Moez Missaoui";
+  moez.age = 29;
+  moez.salary = 2200.950;
+  moez.city = "Aouina";
+  moez.country = "Tunis";
+
+  print("${moez.getName()}, ${moez.getAge()} years old.");
+  print("From ${moez.city} - ${moez.country} .");
+  moez.showSalary();
+  moez.eat();
+  moez.sleep();
+  moez.watch();
 }
