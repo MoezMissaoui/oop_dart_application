9620ec7d1de611512b6a87aabf358539f33fc9cf
Moez Missaoui <moeiz.missaoui@gmail.com>
2022-04-23 14:26:29 +0100

    change name app

---
+++ bin/main.dart
@@ -0,0 +1,6 @@
+import 'package:oop_dart_application/oop_dart_application.dart'
+    as oop_dart_application;
+
+void main(List<String> arguments) {
+  print('Hello world: ${oop_dart_application.calculate()}!');
+}
